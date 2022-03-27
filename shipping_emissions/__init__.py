78530c48bc90e3aa1c2f11c6fc0f4461d48255c7
Chad Stainbank <20968201+stainbank@users.noreply.github.com>
2022-03-27 04:58:54 +0100

    Log when non-xlsx passed

--- shipping_emissions/__init__.py
+++ shipping_emissions/__init__.py
@@ -1,1 +1,5 @@
-__version__ = '0.1.0'
+import logging
+
+__version__ = "0.1.0"
+
+logging.basicConfig(level=logging.INFO)
--- shipping_emissions/pipeline/extract.py
+++ shipping_emissions/pipeline/extract.py
@@ -1,14 +1,20 @@
 from collections.abc import Iterator
 from pathlib import Path
+import logging
 from typing import Optional
 
 import openpyxl
 
 from shipping_emissions.pipeline.schema import XlsxSchema
 
+logger = logging.getLogger(__name__)
 
 def read_xlsx(xlsx_file: Path, schema: XlsxSchema) -> Iterator[tuple[Optional[str], ...]]:
     """Yield rows in `xlsx_file` with non-null values coerced to str."""
+    if xlsx_file.suffix != '.xlsx':
+        logger.warning(f"Skipping {xlsx_file} (not an Excel file)")
+        return
+    logger.info(f"Reading {xlsx_file}")
     # Ideally would use read_only=True but it fails to load the entire workbook
     workbook = openpyxl.load_workbook(xlsx_file)
     try:
