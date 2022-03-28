93d75053a66fa2a24c3724714cfd9f031612dfb8
Chad Stainbank <20968201+stainbank@users.noreply.github.com>
2022-03-28 08:20:50 +0100

    Implement endpoint to query ships

--- shipping_emissions/api/schema.py
+++ shipping_emissions/api/schema.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 import pyarrow as pa
 import pyarrow.compute as pc
 import strawberry
@@ -18,8 +18,14 @@ class DatabaseSession(strawberry.extensions.Extension):
 @strawberry.type
 class Ship:
     data: strawberry.Private[Dict[str, Any]]
-    imo_number: int
-    reporting_period: int
+
+    @strawberry.field
+    def imo_number(self) -> str:
+        return self.data["imo_number"]
+
+    @strawberry.field
+    def reporting_period(self) -> int:
+        return self.data["reporting_period"]
 
     @strawberry.field
     def name(self) -> str:
@@ -35,14 +41,30 @@ class Query:
         reporting_period: int = LATEST_REPORTING_PERIOD,
     ) -> Ship:
         df = info.context["df"]
-        rows = df.filter(
-            pc.and_(
-                pc.equal(df["imo_number"], imo_number),
-                pc.equal(df["reporting_period"], reporting_period),
-            )
-        )
-        (row,) = rows.to_pylist()  # TODO: handle 0 or >1 results
-        return Ship(row, imo_number, reporting_period)
+        # TODO: Get constraints automatically from the context
+        constraints = {"imo_number": imo_number, "reporting_period": reporting_period}
+        df = _filter_df(info.context["df"], **constraints)
+        (row,) = df.to_pylist()  # TODO: handle 0 or >1 results
+        return Ship(row)
+
+    @strawberry.field
+    def ships(
+        info: strawberry.types.Info,
+        imo_number: Optional[int] = None,
+        reporting_period: Optional[int] = None,
+        name: Optional[str] = None,
+    ) -> list[Ship]:
+        constraints = {}
+        df = _filter_df(info.context["df"], **info.variable_values)
+        # TODO: Implement pagination
+        return [Ship(row) for row in df.to_pylist()]
+
+
+def _filter_df(df: pa.Table, **constraints: Dict[str, Any]) -> pa.Table:
+    for field, value in constraints.items():
+        if value is not None:
+            df = df.filter(pc.equal(df[field], value))
+    return df
 
 
 schema = strawberry.Schema(query=Query, extensions=[DatabaseSession])
